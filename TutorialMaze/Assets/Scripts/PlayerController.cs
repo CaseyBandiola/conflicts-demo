5c35da5380583db17f3e424464c03a7f0476bec4
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2020-09-22 20:19:17 +0800

    Implemented Player collision
    Added Rigidbody 2D and Circle Collider 2D to Player. Player can no longer pass through walls in the OnCollisionEnter2D method.

--- TutorialMaze/Assets/Scripts/PlayerController.cs
+++ TutorialMaze/Assets/Scripts/PlayerController.cs
@@ -32,4 +32,26 @@ public class PlayerController : MonoBehaviour
             transform.Translate(0, -speed * Time.deltaTime, 0);
         }
     }
+
+    private void OnCollisionEnter2D( Collision2D collidingObject ){
+        // wall collision
+        if (collidingObject.gameObject.tag == "Walls")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+    }
 }
