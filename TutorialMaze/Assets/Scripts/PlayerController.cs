1f01041ce0fcd36811ab4809cbd10a1e5c6eec12
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2020-09-22 20:13:37 +0800

    Implemented Player movement
    Player can now move inside scene using WASD/Arrow keys.

--- TutorialMaze/Assets/Scripts/PlayerController.cs
+++ TutorialMaze/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        speed = 10.0f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        MovePlayer();
+    }
+
+    private void MovePlayer(){
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
     }
 }
