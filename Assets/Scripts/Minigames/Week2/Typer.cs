b2c0d726688aa1bebb50411bba27608700e6cba3
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2020-12-02 22:41:52 +0800

    Added 2 new functions
    - SetCurrentWord and SetRemainingWord
    - SetCurrentWord is called on Start()

--- Assets/Scripts/Minigames/Week2/Typer.cs
+++ Assets/Scripts/Minigames/Week2/Typer.cs
@@ -7,13 +7,24 @@ public class Typer : MonoBehaviour {
     public Text wordOutput = null;
 
     private string remainingWord = string.Empty;
-    private string currWord = string.Empty;
+    private string currWord = "Test";
 
     private void Start() {
-        
+        SetCurrentWord();
     }
 
     private void Update() {
         
+    }
+
+    // Sets the current word in the screen
+    private void SetCurrentWord(){
+        // Get word from word bank
+        // Update remaining word
+        SetRemainingWord(currWord);
+    }
+
+    private SetRemainingWord(string s){
+
     }
 }
