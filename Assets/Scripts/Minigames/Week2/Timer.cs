4a913c282b6db4770f399ef9bd66f1e0bbe880ef
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2020-12-13 17:40:02 +0800

    Changed text display in Timer.cs
    - Changed from minute:second formatting to seconds only with one decimal point
    - Added a new function to check if time is up

--- Assets/Scripts/Minigames/Week2/Timer.cs
+++ Assets/Scripts/Minigames/Week2/Timer.cs
@@ -10,29 +10,32 @@ public class Timer : MonoBehaviour {
 
     private bool stopTimer;
 
-    // Start is called before the first frame update
     void Start() {
         stopTimer = false;
         timerSlider.maxValue = gameTime;
         timerSlider.value = gameTime;    
     }
 
-    // Update is called once per frame
     void Update() {
         float time = gameTime - Time.time;
 
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time-minutes * 60f);
 
-        string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        // old text formatting
+        // string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
 
         if( time <= 0 ){
             stopTimer = true;
         }
 
         if( !stopTimer ){
-            timerText.text = textTime;
+            timerText.text = time.ToString("0.0");
             timerSlider.value = time;
         }
+    }
+
+    public bool TimeUp(){
+        return stopTimer;
     }
 }
