abe8b7c25034d966285684900718059132e41340
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2020-12-13 17:33:48 +0800

    Finish Timer.cs
    - Reflect change on slider as time passes
    - Change text to current time

--- Assets/Scripts/Minigames/Week2/Timer.cs
+++ Assets/Scripts/Minigames/Week2/Timer.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
-{
+public class Timer : MonoBehaviour {
     public Slider timerSlider;
+    public Text timerText;
+    public float gameTime;
+
+    private bool stopTimer;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        stopTimer = false;
+        timerSlider.maxValue = gameTime;
+        timerslider.value = gameTime;    
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float time = gameTime - Time.time;
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time-minutes * 60f);
+
+        string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        if( time <= 0 ){
+            stopTimer = true;
+        }
+
+        if( !stopTimer ){
+            timerText.text = textTime;
+            timerSlider.value = time;
+        }
     }
 }
