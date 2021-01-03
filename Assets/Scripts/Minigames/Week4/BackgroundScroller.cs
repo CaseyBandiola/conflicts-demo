6268c19138adb0466b2356e6eb100774a4cbec70
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2021-01-04 03:28:38 +0800

    Implement background scrolling for Week 4 minigame

--- Assets/Scripts/Minigames/Week4/BackgroundScroller.cs
+++ Assets/Scripts/Minigames/Week4/BackgroundScroller.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour {
     public BoxCollider2D collider;
-    public RigidBody2D rb;
+    public Rigidbody2D rb;
 
     private float width;
     public float scrollSpeed = -2f;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        // get components from object
+        collider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
         
+        // disable collider after to prevent memory waste
+        width = collider.size.x;
+        collider.enabled = false;
+
+        // scroll to the left
+        rb.velocity = new Vector2(scrollSpeed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( transform.position.x < -width ){
+            Vector2 resetPos = new Vector2(width*2f, 0);
+            transform.position = (Vector2) transform.position + resetPos;
+        }
     }
 }
