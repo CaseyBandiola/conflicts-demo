aec84d93a68f8cd01d4bd0a2ff940e211154020d
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2021-01-04 04:45:32 +0800

    Implemented power up moving code
    - Added more invisible walls

---
+++ Assets/Scripts/Minigames/Week4/PowerUpController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour{
+    private float speed;
+    // Start is called before the first frame update
+    void Start(){
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.x > -13 ){
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
