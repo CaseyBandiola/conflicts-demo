d6e2c2eed66105b5e50eca9f7d89d278c2affa16
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2021-01-04 03:19:18 +0800

    Initialize BackgroundScroller for Week 4 minigame

---
+++ Assets/Scripts/Minigames/Week4/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+    public BoxCollider2D collider;
+    public RigidBody2D rb;
+
+    private float width;
+    public float scrollSpeed = -2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
