33897efbea3652882db4cfe4afe2c14b918f8d0a
Casey Bandiola <john.bandiola@obf.ateneo.edu>
2021-01-04 05:37:12 +0800

    Finish Week 4 minigame
    - Implement PASS/FAIL conditions
    - Removed movement capabilities at game end

--- Assets/Scripts/Minigames/Week4/PlayerController4.cs
+++ Assets/Scripts/Minigames/Week4/PlayerController4.cs
@@ -1,38 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PlayerController4 : MonoBehaviour
-{
+public class PlayerController4 : MonoBehaviour{
     private float speed;
+
+    public Timer4 timer;
+    public bool timeUp;
+    private bool gameEnd;
+
+    public Text gameOver;
     // Start is called before the first frame update
     void Start(){
         speed = 1.8f;
+        timeUp = false;
+        gameEnd = false;
     }
 
     // Update is called once per frame
     void Update() {
-        //float horizontal = Input.GetAxis("Horizontal");
         MovePlayer();
+        // fail conditions
+        if( timer.TimeUp() && !gameEnd ){
+            // stop movement
+            if( timeUp == false ){
+                timeUp = true;
+            }
 
+            // FAIL CODE
+            gameOver.text = "Fail! Press 'Enter' to proceed";
+            // load fail scene
+            if (Input.GetKey(KeyCode.Return)){
+                Loader.Load(Loader.Scene.Week2);
+            }
+
+        }
     }
 
     private void MovePlayer(){
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
+        if( !timeUp ){
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
 
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
-            transform.Translate(0, speed * Time.deltaTime, 0);
-        }
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
-            transform.Translate(0, -speed * Time.deltaTime, 0);
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
         }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collidingObject){
@@ -68,11 +93,21 @@ public class PlayerController4 : MonoBehaviour
             Destroy(collidingObject.gameObject);
         }
 
-    }
-
-    private void OnTriggerEnter2D(Collider2D collidingTrigger){
-        if (collidingTrigger.gameObject.tag == "Exit"){
+        if( collidingObject.gameObject.tag == "Exit" ){
             // end game
+            gameEnd = true;
+            // stop movement
+            if (timeUp == false){
+                timeUp = true;
+            }
+
+            // success code
+            gameOver.text = "Success! Press 'Enter' to proceed";
+
+            // Load success scene
+            if (Input.GetKey(KeyCode.Return)){
+                Loader.Load(Loader.Scene.Week2);
+            }
         }
     }
 
